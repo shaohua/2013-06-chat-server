71e168a564df68a3140d606e7fdf52708a4cdc45
Hack Reactor Students <github@hackreactor.com>
2013-07-08 11:24:38 -0700

    Loading function from another file is working

--- request-handler.js
+++ request-handler.js
@@ -4,6 +4,25 @@
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
 
+// var defaultCorsHeaders = require('./basic-server.js').defaultCorsHeaders;
 var handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
 
+  var statusCode = 200;
+
+  var headers = defaultCorsHeaders;
+
+  headers['Content-Type'] = "text/plain";
+
+  response.writeHead(statusCode, headers);
+  response.end("Hello, World!");
 };
+
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+exports.handleRequest = handleRequest;
