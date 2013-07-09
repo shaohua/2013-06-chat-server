a9e11d4f837f6d64f53b208c52d428e45d8a9e78
Hack Reactor Students <github@hackreactor.com>
2013-07-09 12:38:36 -0700

    Serving index.html from browser works

--- basic-server.js
+++ basic-server.js
@@ -3,8 +3,8 @@ var http = require("http");
 var request_handler = require('./request-handler.js');
 
 var port = 8080;
-var ip = "127.0.0.1";
-// var ip = '10.0.1.19';
+// var ip = "127.0.0.1";
+var ip = '10.0.1.19';
 var server = http.createServer(request_handler.handleRequest);
 
 console.log("Listening on http://" + ip + ":" + port);
